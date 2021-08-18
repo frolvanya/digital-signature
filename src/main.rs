8c423d040c26ba245de93cd8b4b56d91d1ba8b93
UnforgivenLalalend <acc4anim@gmail.com>
2021-08-18 14:05:25 +0200

    added another_message future

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,31 @@ use openssl::symm::Cipher;
 
 use sha2::{Digest, Sha512};
 
-fn sha512_hash(text: &str) -> String {
+fn sha512_hash(text: &str) -> Vec<u8> {
     let mut hasher = Sha512::new();
     hasher.update(text);
 
-    format!("{:x}", hasher.finalize())
+    hasher.finalize().to_vec()
 }
 
-fn encrypt_with_private_key(private_key: Vec<u8>, seed: String, message: &str) -> Vec<u8> {
+fn encrypt_with_private_key(private_key: Vec<u8>, seed: String, message: &[u8]) -> Vec<u8> {
     let rsa = Rsa::private_key_from_pem_passphrase(&private_key, seed.as_bytes()).unwrap();
     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
 
     let _ = rsa
-        .private_encrypt(message.as_bytes(), &mut buf, Padding::PKCS1)
+        .private_encrypt(message, &mut buf, Padding::PKCS1)
         .unwrap();
 
     buf
 }
 
-fn decrypt_with_public_key(public_key: Vec<u8>, data: Vec<u8>) -> String {
+fn decrypt_with_public_key(public_key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     let rsa = Rsa::public_key_from_pem(&public_key).unwrap();
     let mut buf = vec![0; rsa.size() as usize];
 
     let _ = rsa.public_decrypt(&data, &mut buf, Padding::PKCS1).unwrap();
 
-    String::from_utf8(buf)
-        .unwrap()
-        .trim_matches(char::from(0))
-        .to_string()
+    buf
 }
 
 fn main() {
@@ -63,14 +60,28 @@ fn main() {
 
     // Digital Signature Authentication
 
+    // Message = M'
+    let another_message = "Message";
+
+    // Hash(M') = Mh'
+    let another_message_hash = sha512_hash(another_message);
+
     // Decrypt(Signature, PUBLIC_KEY) = Mh'
-    let decrypted_another_message_hash =
+    let mut decrypted_another_message_hash =
         decrypt_with_public_key(public_key, encrypted_message_hash);
 
+    // Remove all trailing zeros
+    decrypted_another_message_hash.retain(|&x| x != 0);
+
     // Check if Mh' = Mh
-    if message_hash == decrypted_another_message_hash {
+    if another_message_hash == decrypted_another_message_hash {
         println!("Digital signature is genuine!");
     } else {
         println!("Digital signature is not genuine!");
     }
+
+    println!(
+        "{:?}\n\n\n{:?}",
+        message_hash, decrypted_another_message_hash
+    );
 }
