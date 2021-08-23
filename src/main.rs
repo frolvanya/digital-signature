b8450b42faf5ded2df81e2449272bf0a34740f45
UnforgivenLalalend <acc4anim@gmail.com>
2021-08-23 22:35:16 +0200

    grouped code into func

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,22 @@ use openssl::symm::Cipher;
 
 use sha2::{Digest, Sha512};
 
-fn sha512_hash(text: &str) -> Vec<u8> {
-    let mut hasher = Sha512::new();
-    hasher.update(text);
+use serde_json::json;
 
-    hasher.finalize().to_vec()
+fn generate_keys() -> (String, Vec<u8>, Vec<u8>) {
+    let rsa = Rsa::generate(2048).unwrap();
+    let seed: String = rand::thread_rng()
+        .sample_iter(&rand::distributions::Alphanumeric)
+        .take(64)
+        .map(char::from)
+        .collect();
+
+    let public_key = rsa.public_key_to_pem().unwrap();
+    let private_key = rsa
+        .private_key_to_pem_passphrase(Cipher::aes_256_cbc(), seed.as_bytes())
+        .unwrap();
+
+    (seed, public_key, private_key)
 }
 
 fn encrypt_with_private_key(private_key: Vec<u8>, seed: String, message: &[u8]) -> Vec<u8> {
@@ -23,7 +34,7 @@ fn encrypt_with_private_key(private_key: Vec<u8>, seed: String, message: &[u8]) 
     buf
 }
 
-fn decrypt_with_public_key(public_key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
+fn decrypt_with_public_key(data: Vec<u8>, public_key: Vec<u8>) -> Vec<u8> {
     let rsa = Rsa::public_key_from_pem(&public_key).unwrap();
     let mut buf = vec![0; rsa.size() as usize];
 
@@ -32,56 +43,62 @@ fn decrypt_with_public_key(public_key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     buf
 }
 
-fn main() {
-    // +---------+-----------+
-    // | MESSAGE | SIGNATURE |
-    // +---------+-----------+
+fn create_signature(message: &str, private_key: Vec<u8>, seed: String) -> serde_json::Value {
+    let signature = encrypt_with_private_key(private_key, seed, &sha512_hash(message));
 
-    let rsa = Rsa::generate(2048).unwrap();
-    let seed: String = rand::thread_rng()
-        .sample_iter(&rand::distributions::Alphanumeric)
-        .take(64)
-        .map(char::from)
-        .collect();
+    json!({
+        "message": message,
+        "signature": signature.iter().map(|element| format!("{:02X}", element)).collect::<Vec<String>>().join(""),
+    })
+}
 
-    let public_key = rsa.public_key_to_pem().unwrap();
-    let private_key = rsa
-        .private_key_to_pem_passphrase(Cipher::aes_256_cbc(), seed.as_bytes())
-        .unwrap();
+fn message_verification(message_with_signature: serde_json::Value, public_key: Vec<u8>) -> bool {
+    let mut decrypted_message_hash = decrypt_with_public_key(
+        hex_to_bytes(message_with_signature["signature"].as_str().unwrap()).unwrap(),
+        public_key,
+    );
 
-    // Message = M
-    let message = "Message";
-
-    // Hash(M) = Mh
-    let message_hash = sha512_hash(message);
-
-    // Encrypt(Mh, PRIVATE_KEY) = Signature
-    let encrypted_message_hash = encrypt_with_private_key(private_key, seed, &message_hash);
-
-    // Digital Signature Authentication
-
-    // Message = M'
-    let another_message = "Message";
-
-    // Hash(M') = Mh'
-    let another_message_hash = sha512_hash(another_message);
-
-    // Decrypt(Signature, PUBLIC_KEY) = Mh'
-    let mut decrypted_another_message_hash =
-        decrypt_with_public_key(public_key, encrypted_message_hash);
-
-    // Remove all trailing zeros
-    decrypted_another_message_hash.retain(|&x| x != 0);
-
-    // Check if Mh' = Mh
-    if another_message_hash == decrypted_another_message_hash {
-        println!("Digital signature is genuine!");
-    } else {
-        println!("Digital signature is not genuine!");
+    while decrypted_message_hash.last().unwrap() == &0 {
+        decrypted_message_hash.pop();
     }
 
-    println!(
-        "{:?}\n\n\n{:?}",
-        message_hash, decrypted_another_message_hash
-    );
+    if sha512_hash(message_with_signature["message"].as_str().unwrap()) == decrypted_message_hash {
+        return true;
+    }
+
+    false
+}
+
+fn sha512_hash(text: &str) -> Vec<u8> {
+    let mut hasher = Sha512::new();
+    hasher.update(text);
+
+    hasher.finalize().to_vec()
+}
+
+fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
+    if s.len() % 2 == 0 {
+        (0..s.len())
+            .step_by(2)
+            .map(|i| {
+                s.get(i..i + 2)
+                    .and_then(|sub| u8::from_str_radix(sub, 16).ok())
+            })
+            .collect()
+    } else {
+        None
+    }
+}
+
+fn main() {
+    let (seed, public_key, private_key) = generate_keys();
+    let message = "Message";
+
+    let message_to_send = create_signature(message, private_key, seed);
+
+    if message_verification(message_to_send, public_key) {
+        println!("Message verification successful!");
+    } else {
+        println!("Message verification failed!");
+    }
 }
