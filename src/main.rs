32dcf9a617fc835e600c3139f9022cb71e187074
UnforgivenLalalend <acc4anim@gmail.com>
2021-08-27 13:40:51 +0200

    feat: few changes in SignedMessage structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,79 @@
-use rand::{self, Rng};
-
 use openssl::rsa::{Padding, Rsa};
-use openssl::symm::Cipher;
-
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha512};
+
+#[derive(Serialize, Deserialize)]
+struct SignedMessage {
+    message: String,
+    signature: String,
+}
+struct PublicKey(Vec<u8>);
+struct PrivateKey(Vec<u8>);
+#[derive(Debug)]
+struct VerificationError(String);
+
+impl SignedMessage {
+    fn new(message: String, private_key: PrivateKey) -> Self {
+        SignedMessage {
+            message: message.clone(),
+            signature: encrypt_with_private_key(&sha512_hash(message.as_str()), private_key)
+                .iter()
+                .map(|element| format!("{:02X}", element))
+                .collect::<Vec<String>>()
+                .join(""),
+        }
+    }
+
+    fn load(
+        message: String,
+        signature: String,
+        public_key: PublicKey,
+    ) -> Result<Self, VerificationError> {
+        let decrypted_message_hash = decrypt_with_public_key(
+            hex_to_bytes(signature.clone().as_str()).unwrap(),
+            public_key,
+        );
+
+        if sha512_hash(message.as_str()) == decrypted_message_hash {
+            return Ok(SignedMessage { message, signature });
+        }
+
+        Err(VerificationError(String::from(
+            "Message verification failed!",
+        )))
+    }
+}
+
+fn generate_keys() -> (PublicKey, PrivateKey) {
+    let rsa = Rsa::generate(2048).unwrap();
+
+    let public_key = PublicKey(rsa.public_key_to_pem().unwrap());
+    let private_key = PrivateKey(rsa.private_key_to_pem().unwrap());
+
+    (public_key, private_key)
+}
+
+fn encrypt_with_private_key(message: &[u8], private_key: PrivateKey) -> Vec<u8> {
+    let rsa = Rsa::private_key_from_pem(&private_key.0).unwrap();
+    let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
+
+    let buf_actual_len = rsa
+        .private_encrypt(message, &mut buf, Padding::PKCS1)
+        .unwrap();
+    buf.truncate(buf_actual_len);
+
+    buf
+}
+
+fn decrypt_with_public_key(data: Vec<u8>, public_key: PublicKey) -> Vec<u8> {
+    let rsa = Rsa::public_key_from_pem(&public_key.0).unwrap();
+    let mut buf = vec![0; rsa.size() as usize];
+
+    let buf_actual_len = rsa.public_decrypt(&data, &mut buf, Padding::PKCS1).unwrap();
+    buf.truncate(buf_actual_len);
+
+    buf
+}
 
 fn sha512_hash(text: &str) -> Vec<u8> {
     let mut hasher = Sha512::new();
@@ -12,76 +82,35 @@ fn sha512_hash(text: &str) -> Vec<u8> {
     hasher.finalize().to_vec()
 }
 
-fn encrypt_with_private_key(private_key: Vec<u8>, seed: String, message: &[u8]) -> Vec<u8> {
-    let rsa = Rsa::private_key_from_pem_passphrase(&private_key, seed.as_bytes()).unwrap();
-    let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
-
-    let _ = rsa
-        .private_encrypt(message, &mut buf, Padding::PKCS1)
-        .unwrap();
-
-    buf
-}
-
-fn decrypt_with_public_key(public_key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
-    let rsa = Rsa::public_key_from_pem(&public_key).unwrap();
-    let mut buf = vec![0; rsa.size() as usize];
-
-    let _ = rsa.public_decrypt(&data, &mut buf, Padding::PKCS1).unwrap();
-
-    buf
+fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
+    if s.len() % 2 == 0 {
+        (0..s.len())
+            .step_by(2)
+            .map(|i| {
+                s.get(i..i + 2)
+                    .and_then(|sub| u8::from_str_radix(sub, 16).ok())
+            })
+            .collect()
+    } else {
+        None
+    }
 }
 
 fn main() {
-    // +---------+-----------+
-    // | MESSAGE | SIGNATURE |
-    // +---------+-----------+
-
-    let rsa = Rsa::generate(2048).unwrap();
-    let seed: String = rand::thread_rng()
-        .sample_iter(&rand::distributions::Alphanumeric)
-        .take(64)
-        .map(char::from)
-        .collect();
-
-    let public_key = rsa.public_key_to_pem().unwrap();
-    let private_key = rsa
-        .private_key_to_pem_passphrase(Cipher::aes_256_cbc(), seed.as_bytes())
-        .unwrap();
-
-    // Message = M
+    let (public_key, private_key) = generate_keys();
     let message = "Message";
 
-    // Hash(M) = Mh
-    let message_hash = sha512_hash(message);
+    let signed_message = SignedMessage::new(message.to_string(), private_key);
 
-    // Encrypt(Mh, PRIVATE_KEY) = Signature
-    let encrypted_message_hash = encrypt_with_private_key(private_key, seed, &message_hash);
-
-    // Digital Signature Authentication
-
-    // Message = M'
-    let another_message = "Message";
-
-    // Hash(M') = Mh'
-    let another_message_hash = sha512_hash(another_message);
-
-    // Decrypt(Signature, PUBLIC_KEY) = Mh'
-    let mut decrypted_another_message_hash =
-        decrypt_with_public_key(public_key, encrypted_message_hash);
-
-    // Remove all trailing zeros
-    decrypted_another_message_hash.retain(|&x| x != 0);
-
-    // Check if Mh' = Mh
-    if another_message_hash == decrypted_another_message_hash {
-        println!("Digital signature is genuine!");
-    } else {
-        println!("Digital signature is not genuine!");
+    match SignedMessage::load(
+        signed_message.message.to_string(),
+        signed_message.signature,
+        public_key,
+    ) {
+        Ok(message_with_signature) => println!(
+            "Message was successfully verificated!\nMessage: {}\nSignature: {}",
+            message_with_signature.message, message_with_signature.signature
+        ),
+        Err(e) => println!("Message verification failed!\nError: {:?}", e),
     }
-
-    println!(
-        "{:?}\n\n\n{:?}",
-        message_hash, decrypted_another_message_hash
-    );
 }
